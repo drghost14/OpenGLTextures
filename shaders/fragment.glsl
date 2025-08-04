78b2b097c959c4ff98ae9bdc2b1883d35ddf47ca
DRG_Games <dr.ghost1419@gmail.com>
2025-08-04 23:59:51 +0500

    Update fragment.glsl

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,11 +1,10 @@
 #version 330 core
-layout (location = 0) in vec2 aPos;
-layout (location = 1) in vec2 aTexCoords;
+out vec4 FragColor;
 
-out vec2 tex_coords;
+in vec2 tex_coords;
+uniform sampler2D uTexture;
 
 void main ()
 {
-    gl_Position = vec4 (aPos, 0.0, 1.0);
-    tex_coords  = aTexCoords;
+    FragColor = texture (uTexture, tex_coords);
 }
